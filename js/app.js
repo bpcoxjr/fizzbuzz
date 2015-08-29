b186a1cff0c7fc18b5ff1eb8eae6bab762208ca1
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2015-08-29 11:34:09 -0400

    refactored

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ $(document).ready(function(){
 	//Collects the user input upon click
 	$('.print').on('click', function() {
 		var inputValue = $('.user-input').val();
-		inputValue = parseInt(inputValue, 10);
+		inputValue = parseInt(inputValue);
 
 	//Checks to be sure input value is 1) a number and 2)between 1-100
 	if (inputValue % 1 === 0 && inputValue >= 1 && inputValue <= 100) {
 		fizzbuzz(inputValue);
 	}
+	//if user inputs a number less than 1 or greater than 100 they are asked to try again & input field is cleared
 	else {
 		alert("The number you enter must be between 1 and 100! Try again, please!");
 		$('.user-input').val ("")
@@ -21,7 +22,7 @@ $(document).ready(function(){
 
 
 
-
+//function starts counting at 1 and stops at the number entered by user (max)
 function fizzbuzz(max) {
 for (i = 1; i <= max; i++) {
 	console.log(i);
